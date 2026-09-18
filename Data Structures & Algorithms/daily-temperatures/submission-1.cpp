803558ab89adc69760ad787ed814d675b81b56c1
Kashyap Patel <98868287+DeAtHfIrE26@users.noreply.github.com>
2026-09-18 12:51:17 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> result(n, 0), st;
+
+        for (int i = 0; i < n; i++){
+            while (!st.empty() && temperatures[i] > temperatures[st.back()]){
+                int j = st.back(); st.pop_back();
+                result[j] = i - j;
+            }
+            st.push_back(i);
+        }
+        return result;
+    }
+};
